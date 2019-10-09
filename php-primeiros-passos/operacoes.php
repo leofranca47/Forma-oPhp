646161c351dccd292eed420112378b626644c37b
leofranca47 <leoprofissional@live.com>
2019-10-09 09:16:47 -0300

    update 09/10/2019

---
+++ php-primeiros-passos/operacoes.php
@@ -0,0 +1,10 @@
+<?php
+
+$soma = 2 + 2;
+$diminuir = 2 - 2;
+$multiplicar = 2 * 2;
+$dividir = 2 / 2;
+$exponenciacao = 2 ** 3;
+$resto = 2 % 2;
+
+echo $exponenciacao;
---
+++ php-primeiros-passos/tipos.php
@@ -0,0 +1,8 @@
+<?php
+
+$inteiro = 2;
+$decimal = 2.5;
+$caractere = "a";
+$trueOrFalse = true;
+
+echo gettype($caractere);
